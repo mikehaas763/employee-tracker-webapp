be79709ad3d3f7d80d7f4523aba08123af7cc8c6
Mike Haas <mikehaas763@gmail.com>
2016-11-24 23:10:34 -0500

    Generate component for app toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './core/app-toolbar/app-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent
   ],
   imports: [
     BrowserModule,
